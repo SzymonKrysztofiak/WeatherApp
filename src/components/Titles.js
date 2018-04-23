467fadd7b0ce5b66528d02b4a4a5ce9b9fc00a6e
Szymon Krysztofiak <s.krysztofiak@outlook.com>
2018-04-23 16:29:17 +0200

    initial commit create-react-app components skeleton

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>WeatherApp</h1>
+                <p>Sprawdź prognozę pogody we wskazanym mieście...</p>
+            </div>
+        );
+    }
+};
+
+export default Titles;
