49e933e179da49efbe08194fc4f7d4e07d5e25ae
Szymon Krysztofiak <s.krysztofiak@outlook.com>
2018-05-10 23:44:32 +0200

    remove cities.JSON, add google API for coords

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import cities from "../cities";
+import api from "../api";
 
 class Form extends React.Component {
     cityRef = React.createRef();
@@ -24,21 +24,30 @@ class Form extends React.Component {
     getCoords = async event => {
         event.preventDefault();
         console.log(this.cityRef.current.value);
-        const city = cities.find(element => {
-            return element.name === this.cityRef.current.value;
-        });
+        // const city = cities.find(element => {
+        //     return element.name === this.cityRef.current.value;
+        // });
         if (this.cityRef.current.value === "") {
             this.getGeoCoords();
-        } else if (!city) {
-            console.log(
-                `Napisz ładnie jak człowiek, poprawnie... z wielkiej litery itd.`
-            );
+            // } else if (!city) {
+            //     console.log(
+            //         `Napisz ładnie jak człowiek, poprawnie... z wielkiej litery itd.`
+            //     );
             // console.log(coords.lat);
         } else {
-            const coords = await {
-                lat: city.lat,
-                lng: city.lng
+            const api_call = await fetch(
+                `https://maps.googleapis.com/maps/api/geocode/json?address=${
+                    this.cityRef.current.value
+                }&key=${api.goog_key}`
+            );
+            console.log(api_call);
+            const data = await api_call.json();
+            console.log(data);
+            const coords = {
+                lat: data.results[0].geometry.location.lat,
+                lng: data.results[0].geometry.location.lng
             };
+            console.log(coords);
             this.props.addCoordsToState(coords);
         }
     };
