7dd3c5e0bd51d7ca133efd44a4bfd50142be0aeb
Szymon Krysztofiak <s.krysztofiak@outlook.com>
2018-08-11 11:59:12 +0200

    api shell

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # WeatherApp
 
+![alt text](https://raw.githubusercontent.com/SzymonKrysztofiak/WeatherApp/master/WeatherApp.jpg "WheatherApp")
+
 Used:
 
-*   create-react-app
-*   openweathermap API
-*   Google geocoding
-*   moment.js
+- create-react-app
+- openweathermap API
+- Google geocoding
+- moment.js
 
-LIVE: https://szymonkrysztofiak.github.io/WeatherApp
+1. `git clone git@github.com:SzymonKrysztofiak/WeatherApp.git`
+2. `npm install`
+3. Get your own Google API key from: (https://developers.google.com/maps/documentation/geocoding/get-api-key)
+4. Copy API key and paste to `src > api.json` in `goog_key` line
+5. Register own account on (https://openweathermap.org/) and get free API key
+6. Paste API key to `src > api.json` in `owm_key`
+7. `npm start`
--- src/App.js
+++ src/App.js
@@ -1,136 +1,132 @@
-import React from "react";
-import moment from "moment";
-import Titles from "./components/Titles";
-import Form from "./components/Form";
-import Weather from "./components/Weather";
-import Footer from "./components/Footer";
-import api from "./api";
+import React from "react"
+import moment from "moment"
+import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
+import Footer from "./components/Footer"
+import api from "./api"
 
 class App extends React.Component {
-    state = {
-        coordis: {},
-        current: {
-            date: undefined,
-            temperature: undefined,
-            city: undefined,
-            country: undefined,
-            description: undefined
-        },
-        forecast: [],
-        errorMessage: "",
-        isLoaded: true
-    };
+  state = {
+    coordis: {},
+    current: {
+      date: undefined,
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      description: undefined
+    },
+    forecast: [],
+    errorMessage: "",
+    isLoaded: true
+  }
 
-    addCoordsToState = coords => {
-        const coordis = { ...this.state.coordis };
-        coordis["coords"] = coords;
+  addCoordsToState = coords => {
+    const coordis = { ...this.state.coordis }
+    coordis["coords"] = coords
+    this.setState({
+      coordis
+    })
+    this.updateWeatherState()
+    this.forecastState()
+  }
+
+  toggleLoader = () => {
+    this.setState(state => ({ isLoaded: !state.isLoaded }))
+  }
+
+  addError = error => {
+    const errorMessage = { ...this.state.errorMessage }
+    errorMessage["error"] = error
+    this.setState({
+      coordis: {},
+      current: {
+        date: undefined,
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        description: undefined
+      },
+      errorMessage: errorMessage
+    })
+    this.toggleLoader()
+  }
+
+  updateWeatherState = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${
+        this.state.coordis.coords.lat
+      }&lon=${this.state.coordis.coords.lng}&appid=${api.owm_key}&units=metric`
+    )
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        const newDate = moment(data.dt * 1000).format("l LT")
         this.setState({
-            coordis
-        });
-        this.updateWeatherState();
-        this.forecastState();
-    };
+          current: {
+            temperature: data.main.temp,
+            date: newDate,
+            city: data.name,
+            country: data.sys.country,
+            description: data.weather[0].description
+          },
+          errorMessage: ""
+        })
+      })
+    this.toggleLoader()
+  }
 
-    toggleLoader = () => {
-        this.setState(state => ({ isLoaded: !state.isLoaded }));
-    };
-
-    addError = error => {
-        const errorMessage = { ...this.state.errorMessage };
-        errorMessage["error"] = error;
+  // Nowy bajer do opierdolenia
+  forecastState = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${
+        this.state.coordis.coords.lat
+      }&lon=${this.state.coordis.coords.lng}&appid=${api.owm_key}&units=metric`
+    )
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
         this.setState({
-            coordis: {},
-            current: {
-                date: undefined,
-                temperature: undefined,
-                city: undefined,
-                country: undefined,
-                description: undefined
-            },
-            errorMessage: errorMessage
-        });
-        this.toggleLoader();
-    };
+          forecast: data.list.map(item => {
+            return {
+              temperature: item.main.temp,
+              date: item.dt,
+              day: moment(item.dt * 1000).format("dddd"),
+              description: item.weather[0].description
+            }
+          })
+        })
+      })
+  }
 
-    updateWeatherState = () => {
-        fetch(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${
-                this.state.coordis.coords.lat
-            }&lon=${this.state.coordis.coords.lng}&appid=${
-                api.key
-            }&units=metric`
-        )
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                const newDate = moment(data.dt * 1000).format("l LT");
-                this.setState({
-                    current: {
-                        temperature: data.main.temp,
-                        date: newDate,
-                        city: data.name,
-                        country: data.sys.country,
-                        description: data.weather[0].description
-                    },
-                    errorMessage: ""
-                });
-            });
-        this.toggleLoader();
-    };
-
-    // Nowy bajer do opierdolenia
-    forecastState = () => {
-        fetch(
-            `https://api.openweathermap.org/data/2.5/forecast?lat=${
-                this.state.coordis.coords.lat
-            }&lon=${this.state.coordis.coords.lng}&appid=${
-                api.key
-            }&units=metric`
-        )
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                this.setState({
-                    forecast: data.list.map(item => {
-                        return {
-                            temperature: item.main.temp,
-                            date: item.dt,
-                            day: moment(item.dt * 1000).format("dddd"),
-                            description: item.weather[0].description
-                        };
-                    })
-                });
-            });
-    };
-
-    render() {
-        return (
-            <React.Fragment>
-                <div className="bg-container" />
-                <div className="flex-container">
-                    <Titles />
-                    <Form
-                        addCoordsToState={this.addCoordsToState}
-                        addError={this.addError}
-                        toggleLoader={this.toggleLoader}
-                    />
-                    <Weather
-                        temperature={this.state.current.temperature}
-                        date={this.state.current.date}
-                        city={this.state.current.city}
-                        country={this.state.current.country}
-                        description={this.state.current.description}
-                        errorMessage={this.state.errorMessage.error}
-                        isLoaded={this.state.isLoaded}
-                        forecast={this.state.forecast}
-                    />
-                    <Footer />
-                </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="bg-container" />
+        <div className="flex-container">
+          <Titles />
+          <Form
+            addCoordsToState={this.addCoordsToState}
+            addError={this.addError}
+            toggleLoader={this.toggleLoader}
+          />
+          <Weather
+            temperature={this.state.current.temperature}
+            date={this.state.current.date}
+            city={this.state.current.city}
+            country={this.state.current.country}
+            description={this.state.current.description}
+            errorMessage={this.state.errorMessage.error}
+            isLoaded={this.state.isLoaded}
+            forecast={this.state.forecast}
+          />
+          <Footer />
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
+export default App
